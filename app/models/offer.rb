7f230a6f001a55c0db7d6cfd73603e77f3d0f265
Tito Romao <titoromao@hotmail.com>
2022-05-25 16:48:38 +0100

    cloduinary done

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,8 @@ class Offer < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :user
 
+  has_one_attached :photo
+
   validates :price, presence: true
   validates :title, presence: true
   validates :description, presence: true
