eed14844835a836477ee184bda9ad007fb059c49
delio carvalho <delio_carvalho@hotmail.com>
2022-05-26 15:15:54 +0100

    edit running well

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -12,7 +12,7 @@ class OffersController < ApplicationController
   end
 
   def create
-    @offer = Offer.new(create_params)
+    @offer = Offer.new(offer_params)
     @offer.user = current_user
     if @offer.save
       redirect_to offer_path(@offer)
@@ -21,13 +21,34 @@ class OffersController < ApplicationController
     end
   end
 
+  def edit
+    @offer = Offer.find(params[:id])
+  end
+
+  def update
+    @offer = Offer.find(params[:id])
+    if @offer.update(offer_params)
+      redirect_to my_offers_path
+    else
+      render :edit
+    end
+  end
+
   def my_offers
     @offers = current_user.offers
   end
 
+  def destroy_my_offers
+    @offers = current_user.offers
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+
+    redirect_to my_offers_path(@offers)
+  end
+
   private
 
-  def create_params
+  def offer_params
     params.require(:offer).permit(:photo, :title, :description, :price, :duration, :location)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create, :new]
   end
 
-  get 'my_offers', to: 'offers#my_offers'
+  get 'my_offers', to: 'offers#my_offers', as: :my_offers
 end
